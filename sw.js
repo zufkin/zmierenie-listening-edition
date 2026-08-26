def5624a3971e72e73acdd48ad79a549521c0455
zufkin <zufkin@gmail.com>
2026-08-26 20:20:14 +0200

    Refresh UTF-8 shell cache

--- sw.js
+++ sw.js
@@ -1,13 +1,12 @@
-﻿const SHELL='zmierenie-shell-v2';
-const AUDIO='zmierenie-downloads-v2';
+const SHELL='zmierenie-shell-v3';
+const AUDIO='zmierenie-downloads-v3';
 const FILES=["./", "./index.html", "./styles.css", "./app.js", "./manifest.json", "./app.webmanifest", "./icon-192.png", "./icon-512.png"];
 self.addEventListener('install',event=>event.waitUntil(caches.open(SHELL).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('zmierenie-shell-')&&k!==SHELL).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET') return;
   event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{
-    if(response.ok && new URL(event.request.url).pathname.includes('/audio/')) caches.open(AUDIO).then(cache=>cache.put(event.request,response.clone()));
+    if(response.ok && /zmierenie-\d{2}-(pribeh|porozumiet|praktizovat)\.mp3$/.test(new URL(event.request.url).pathname)) caches.open(AUDIO).then(cache=>cache.put(event.request,response.clone()));
     return response;
   })));
 });
-
